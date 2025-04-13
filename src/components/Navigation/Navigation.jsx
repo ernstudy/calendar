191d834e1efd7cc9f55401bc601b60df94894260
ernstudy <hey@ernstudy.com>
2025-04-12 22:53:03 -0400

    create and style navigation

---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,91 @@
+import React, { useEffect, useState } from "react";
+import NavigationLink from "./NavigationLink";
+import NavigationStyle from "./Navigation.module.css";
+
+// icons
+import {
+  ArrowRight,
+  CalendarMonth,
+  DeleteOutline,
+  FavoriteBorder,
+  FormatListBulleted,
+  Home,
+} from "@mui/icons-material";
+
+export default function Navigation() {
+  const [url, setUrl] = useState({
+    navUrl: "",
+    currentUrl: window.location.pathname,
+  });
+
+  const navLinkItems = [
+    {
+      icon: <Home className={NavigationStyle.icon} />,
+      text: "Home",
+      path: "/",
+    },
+    {
+      icon: <FormatListBulleted className={NavigationStyle.icon} />,
+      text: "Event list",
+      path: "/event-list",
+    },
+    {
+      icon: <CalendarMonth className={NavigationStyle.icon} />,
+      text: "Calendar",
+      path: "/calendar",
+    },
+    {
+      icon: <FavoriteBorder className={NavigationStyle.icon} />,
+      text: "Favorite",
+      path: "/favorite",
+    },
+    {
+      icon: <DeleteOutline className={NavigationStyle.icon} />,
+      text: "Trash",
+
+      path: "/trash",
+    },
+  ];
+
+  const activeLink = () => {
+    const currentButton = navLinkItems.find(
+      (item) => item.path == url.currentUrl
+    );
+
+    if (currentButton) {
+      setUrl((prevValue) => ({
+        ...prevValue,
+        navUrl: currentButton.path,
+      }));
+      console.log("current button path", currentButton.path);
+      console.log("currentUrl:", url.currentUrl);
+    }
+  };
+
+  const handleActiveLink = (url) => {
+    setUrl((prevValue) => ({ ...prevValue, navUrl: url }));
+  };
+
+  useEffect(() => {
+    activeLink();
+  }, [url.currentUrl]);
+
+  return (
+    <div className={NavigationStyle.navigationBar}>
+      <div className={NavigationStyle.navigationList}>
+        {navLinkItems.map((item, index) => (
+          <NavigationLink
+            linkProps={{
+              handleActiveLink,
+              url,
+              setUrl,
+              navLinkItems,
+              index,
+              item,
+            }}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
---
+++ src/components/Navigation/NavigationLink.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import navigationStyle from "./Navigation.module.css";
+import clsx from "clsx";
+import { ArrowRight } from "@mui/icons-material";
+
+export default function NavigationLink({ linkProps }) {
+  const { handleActiveLink, url, index, item } = linkProps;
+  console.log(linkProps.index);
+  return (
+    <>
+      <Link
+        to={item.path}
+        className={clsx(
+          navigationStyle.linkButton,
+          url.navUrl == item.path && navigationStyle["linkButton--active"]
+        )}
+        onClick={() => handleActiveLink(item.path)}
+        key={index}
+      >
+        <div className={navigationStyle.items}>
+          <div className={navigationStyle.icons}>{item.icon}</div>
+          <div className={navigationStyle.text}>{item.text}</div>
+        </div>
+        <div
+          className={navigationStyle.rightIcon}
+          style={{
+            display: url.navUrl == item.path ? "block" : "none",
+          }}
+        >
+          <ArrowRight className={navigationStyle.icon} />
+        </div>
+      </Link>
+    </>
+  );
+}
