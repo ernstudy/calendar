faeb14a42ae84682d489d348272512a6b1da80b4
ernstudy <hey@ernstudy.com>
2025-04-19 20:08:29 -0400

    declare functions to edite, to delete, to add event to favorite and to change event status

--- src/components/EventComponent/EventMenu/EventMenu.jsx
+++ src/components/EventComponent/EventMenu/EventMenu.jsx
@@ -1,17 +1,55 @@
 import React, { useState } from "react";
 import EventMenuStyles from "./EventMenu.module.css";
 import {
+  Cancel,
   Delete,
-  Done,
+  DoneAllOutlined,
   Edit,
   FavoriteBorder,
   MoreVertSharp,
+  Pending,
 } from "@mui/icons-material";
 import clsx from "clsx";
 import { useEventContext } from "../../../context/EventContext";
 
-export default function EventMenu({ id, eventId }) {
-  const { eventClick, isEventClicked } = useEventContext();
+export default function EventMenu({ eventId, isEventId, event }) {
+  const {
+    eventClick,
+    events,
+    setEvents,
+    deleteEvent,
+    moveToTrash,
+    addToFavorite,
+    eventModalToggle,
+    setEventToEdit,
+    updateEventStatus,
+  } = useEventContext();
+
+  const handleDeleteEvent = () => {
+    const currentEvent = events.find((event) => event.id == eventId);
+    if (currentEvent) {
+      moveToTrash(currentEvent);
+      deleteEvent(currentEvent);
+    }
+  };
+
+  const handleEditEvent = () => {
+    eventModalToggle();
+    const currentEvent = events.find((event) => event.id == eventId);
+    setEventToEdit(currentEvent);
+  };
+
+  const handleEventStatus = (status) => {
+    updateEventStatus(status, eventId);
+  };
+
+  const handleFavorite = () => {
+    const currentEvent = events.find((event) => event.id == eventId);
+    if (currentEvent) {
+      addToFavorite(currentEvent);
+    }
+  };
+
   return (
     <>
       {/* menu  */}
@@ -23,28 +61,67 @@ export default function EventMenu({ id, eventId }) {
         <ul
           className={clsx(
             EventMenuStyles.menuList,
-            isEventClicked && id == eventId
-              ? EventMenuStyles["menuList--open"]
-              : ""
+            eventId == isEventId && EventMenuStyles["menuList--open"]
           )}
           role="menu"
         >
-          <li className={EventMenuStyles.menuItem} role="menuitem">
+          <li
+            className={EventMenuStyles.menuItem}
+            role="menuitem"
+            onClick={handleEditEvent}
+          >
             {/* delete icon */}
             <Edit className={EventMenuStyles.menuItem__icon} />
             Edit
           </li>
-          <li className={EventMenuStyles.menuItem} role="menuitem">
-            {/* done icon */}
-            <Done className={EventMenuStyles.menuItem__icon} />
-            Done
-          </li>
-          <li className={EventMenuStyles.menuItem} role="menuitem">
+
+          {event.status != "Pending" ? (
+            <li
+              className={EventMenuStyles.menuItem}
+              role="menuitem"
+              onClick={() => handleEventStatus("Pending")}
+            >
+              {/* done icon */}
+              <Pending className={EventMenuStyles.menuItem__icon} />
+              Pending
+            </li>
+          ) : (
+            <>
+              <li
+                className={EventMenuStyles.menuItem}
+                role="menuitem"
+                onClick={() => handleEventStatus("Completed")}
+              >
+                {/* done icon */}
+                <DoneAllOutlined className={EventMenuStyles.menuItem__icon} />
+                Complete
+              </li>
+              <li
+                className={EventMenuStyles.menuItem}
+                role="menuitem"
+                onClick={() => handleEventStatus("Canceled")}
+              >
+                {/* done icon */}
+                <Cancel className={EventMenuStyles.menuItem__icon} />
+                Cancel
+              </li>
+            </>
+          )}
+
+          <li
+            className={EventMenuStyles.menuItem}
+            role="menuitem"
+            onClick={handleFavorite}
+          >
             {/* favorite icon */}
             <FavoriteBorder className={EventMenuStyles.menuItem__icon} />
             Favorite
           </li>
-          <li className={EventMenuStyles.menuItem} role="menuitem">
+          <li
+            className={EventMenuStyles.menuItem}
+            role="menuitem"
+            onClick={handleDeleteEvent}
+          >
             {/* delete icon */}
             <Delete className={EventMenuStyles.menuItem__icon} />
             Delete
